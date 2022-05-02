d52c9d321305c73ba07d97758c73f3b00352d5b1
Radovan Blaha <kedar.blaha@gmail.com>
2022-05-03 00:47:50 +0200

    Made script automatically link all files from config folder

--- install.sh
+++ install.sh
@@ -1,16 +1,36 @@
-#!/bin/sh
-
+#!/bin/bash
+shopt -s dotglob
 DOTFILES=$(dirname $(readlink -f $0))
+DOTFILES="$DOTFILES/config"
+
+handleFile() {
+    for item in "$DOTFILES"/"$1"/*; do
+    if [ "$1" == "$item" ]; then
+        continue
+    fi
+
+    filename=$(basename "$item")
+    
+    if [ -d "$item" ]; then
+        [ ! -d "$HOME""$1"/"$filename" ] && mkdir "$HOME""$1"/"$filename"
+        handleFile "$1"/"$filename"
+    else 
+       ln -sf "$DOTFILES""$1"/"$filename" "$HOME""$1"/"$filename"
+    fi
+done
+}
+
 # todo make more modular not hardcoded, remove . before files in dotfiles
 
-sudo rm -rf ~/.bashrc
-sudo rm -rf ~/.config/kitty
-sudo rm -rf ~/.config/nvim
+#sudo rm -rf ~/.bashrc
+#sudo rm -rf ~/.config/kitty
+#sudo rm -rf ~/.config/nvim
 
-ln -s $DOTFILES/.bashrc $HOME/.bashrc
-mkdir $HOME/.config/kitty
-ln -s $DOTFILES/.config/kitty/kitty.conf $HOME/.config/kitty/kitty.conf 
-ln -s $DOTFILES/.config/kitty/current-theme.conf $HOME/.config/kitty/current-theme.conf
-mkdir $HOME/.config/nvim 
-ln -s $DOTFILES/.config/nvim/init.lua $HOME/.config/nvim/init.lua
-ln -fs $DOTFILES/.zshrc $HOME/.zshrc
+#ln -s $DOTFILES/.bashrc $HOME/.bashrc
+#mkdir $HOME/.config/kitty
+#ln -s $DOTFILES/.config/kitty/kitty.conf $HOME/.config/kitty/kitty.conf 
+#ln -s $DOTFILES/.config/kitty/current-theme.conf $HOME/.config/kitty/current-theme.conf
+#mkdir $HOME/.config/nvim 
+#ln -s $DOTFILES/.config/nvim/init.lua $HOME/.config/nvim/init.lua
+#ln -fs $DOTFILES/.zshrc $HOME/.zshrc
+handleFile ""
