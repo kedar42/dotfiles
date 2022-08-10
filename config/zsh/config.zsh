4edb0913c727ff375a2170288808072eb3901adc
Radovan Blaha <kedar.blaha@gmail.com>
2022-08-10 20:27:01 +0200

    Small fixup, integrated up function

--- config/zsh/config.zsh
+++ config/zsh/config.zsh
@@ -63,14 +63,14 @@ unsetopt CHECK_JOBS       # Don't report on jobs when shell exit.
 
 ## History
 HISTFILE="$CACHE/zhistory"
-HISTSIZE=100000   # Max events to store in internal history.
+HISTSIZE=120000   # Max events to store in internal history.
 SAVEHIST=100000   # Max events to store in history file.
 
 setopt BANG_HIST                 # Don't treat '!' specially during expansion.
 setopt EXTENDED_HISTORY          # Include start time in history records
 setopt APPEND_HISTORY            # Appends history to history file on exit
 setopt INC_APPEND_HISTORY        # Write to the history file immediately, not when the shell exits.
-setopt SHARE_HISTORY             # Share history between all sessions.
+setopt SHARE_HISTORY             # Share historyS between all sessions.
 setopt HIST_EXPIRE_DUPS_FIRST    # Expire a duplicate event first when trimming history.
 setopt HIST_IGNORE_DUPS          # Do not record an event that was just recorded again.
 setopt HIST_IGNORE_ALL_DUPS      # Remove old events if new event is a duplicate
@@ -80,6 +80,7 @@ setopt HIST_SAVE_NO_DUPS         # Do not write a duplicate event to the history
 setopt HIST_REDUCE_BLANKS        # Minimize unnecessary whitespace
 setopt HIST_VERIFY               # Do not execute immediately upon history expansion.
 setopt HIST_BEEP                 # Beep when accessing non-existent history.
+setopt HIST_FCNTL_LOCK           # Use modern file-locking mechanisms, for better safety & performance.
 
 ## Directories
 DIRSTACKSIZE=9
