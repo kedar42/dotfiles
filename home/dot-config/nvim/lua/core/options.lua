f1504c8b97527a032fc20a325b83a77a709452bd
Radovan Blaha <kedar.blaha@outlook.com>
2025-01-07 11:08:41 +0100

    fixed p10k loading and addet nvim

---
+++ home/dot-config/nvim/lua/core/options.lua
@@ -0,0 +1,11 @@
+-- Basic settings
+vim.opt.number = true         -- Show line numbers
+vim.opt.expandtab = true     -- Use spaces instead of tabs
+vim.opt.tabstop = 4          -- Number of spaces tabs count for
+vim.opt.shiftwidth = 4       -- Size of indent
+vim.opt.smartindent = true   -- Auto indent new lines
+vim.opt.wrap = false          -- Don't wrap long lines
+vim.opt.clipboard = "unnamedplus"  -- Use system clipboard
+vim.opt.mouse = "a"               -- Enable mouse support
+vim.opt.termguicolors = true
+vim.opt.showmode = false
