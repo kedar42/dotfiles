25fb7b66db224bee1efab8c6167d53aec2ad581a
Radovan Blaha <kedar.blaha@outlook.com>
2025-03-01 19:31:47 +0100

    Added fish config

---
+++ home/dot-config/fish/conf.d/setup.fish
@@ -0,0 +1,14 @@
+function fish_greeting
+    #if type -q neofetch
+    #    neofetch
+    #end
+end
+
+fish_add_path /opt/homebrew/bin
+
+function fish_user_key_bindings
+    bind \t complete
+end
+
+starship init fish | source
+enable_transience
---
+++ home/dot-config/fish/config.fish
@@ -0,0 +1,7 @@
+
+
+# TODO: End non interactive shell here
+
+# set -x SSH_AUTH_SOCK /Users/$USER/.bitwarden-ssh-agent.sock
+
+set -x SSH_AUTH_SOCK /Users/$USER/Library/Containers/com.maxgoedjen.Secretive.SecretAgent/Data/socket.ssh
